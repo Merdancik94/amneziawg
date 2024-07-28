4b3d434ab85a26e44a25ec5ba6527866487f975e
Merdancik94 <97890006+Merdancik94@users.noreply.github.com>
2024-07-29 02:10:41 +0500

    Update amneziawg+monitoring.sh

--- amneziawg+monitoring.sh
+++ amneziawg+monitoring.sh
@@ -37,4 +37,99 @@ function checkOS() {
         OS="fedora"
     elif [ -e /etc/centos-release ]; then
         OS="centos"
-    elif [ -e /etc/almali
+    elif [ -e /etc/almalinux-release ]; then
+        OS="almalinux"
+    elif [ -e /etc/rocky-release ]; then
+        OS="rocky"
+    else
+        echo -e "${RED}Your distribution is not supported (yet).${NC}"
+        exit 1
+    fi
+}
+
+function getHomeDirForClient() {
+    # Your existing implementation or placeholder
+    # For example:
+    # echo "/home/$(getent passwd $1 | cut -d: -f6)"
+    # Note: Ensure this function returns the appropriate home directory path.
+}
+
+function initialCheck() {
+    isRoot
+    checkVirt
+    checkOS
+}
+
+function readJminAndJmax() {
+    # Your existing implementation or placeholder
+}
+
+function readS1AndS2() {
+    # Your existing implementation or placeholder
+}
+
+function readH1AndH2AndH3AndH4() {
+    # Your existing implementation or placeholder
+}
+
+function installQuestions() {
+    # Your existing implementation or placeholder
+}
+
+function installApacheMonitoring() {
+    echo -e "${GREEN}Installing Apache monitoring tools...${NC}"
+    if [[ ${OS} == 'ubuntu' || ${OS} == 'debian' ]]; then
+        apt update
+        apt install -y apache2 apache2-utils
+        a2enmod status
+    elif [[ ${OS} == 'fedora' || ${OS} == 'centos' || ${OS} == 'almalinux' || ${OS} == 'rocky' ]]; then
+        dnf install -y httpd httpd-tools
+        echo "LoadModule status_module modules/mod_status.so" >> /etc/httpd/conf.modules.d/00-base.conf
+    fi
+}
+
+function configureModStatus() {
+    echo -e "${GREEN}Configuring Apache mod_status...${NC}"
+    if [[ ${OS} == 'ubuntu' || ${OS} == 'debian' ]]; then
+        echo "
+        <Location /server-status>
+            SetHandler server-status
+            Require local
+        </Location>
+        " > /etc/apache2/conf-available/status.conf
+        a2enconf status
+        systemctl restart apache2
+    elif [[ ${OS} == 'fedora' || ${OS} == 'centos' || ${OS} == 'almalinux' || ${OS} == 'rocky' ]]; then
+        echo "
+        <Location /server-status>
+            SetHandler server-status
+            Require host example.com
+            Require ip 192.168.0.0/16
+        </Location>
+        " > /etc/httpd/conf.d/status.conf
+        systemctl restart httpd
+    fi
+}
+
+function installAmneziaWG() {
+    # Run setup questions first
+    installQuestions
+
+    # Existing installation steps for AmneziaWG
+    if [[ ${OS} == 'ubuntu' || ${OS} == 'debian' ]]; then
+        apt update
+        apt install -y somepackage
+    elif [[ ${OS} == 'fedora' || ${OS} == 'centos' || ${OS} == 'almalinux' || ${OS} == 'rocky' ]]; then
+        dnf install -y somepackage
+    fi
+
+    # Install and configure Apache monitoring
+    installApacheMonitoring
+    configureModStatus
+
+    echo -e "${GREEN}AmneziaWG and Apache monitoring setup completed.${NC}"
+}
+
+# Main execution
+initialCheck
+installAmneziaWG
